2df36c3bbf7e15fcef31c8f95e3ddaef6bda8a26
aditya agrawal <f20201381@goa.bits-pilani.ac.in>
2021-03-04 09:21:22 +0530

    animation-working

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 var holder = document.getElementById('main_span');
-var text = ['Welcome to my webpage.','Scroll down for required links.','What you waiting for?','Go.'];
+var text = ['Welcome to my webpage.','Scroll down for required links.','What are you waiting for?','Go.'];
 var t = text.length;
-var location = [0,0];
+var l = [0,0];
+var flag = 0;
+var c = 0;
+var f = 0;
 var sen = text.length;
 function loop(){
     var len = 0;
-    if(location[0]>=sen){
-        location[1] = 0;
-        len = 1;    
+    len = text[l[0]].length;
+    holder.style.width = String(l[1]*100/len)+'%';
+    l[1]++;
+    if (flag ==1){
+        l[1]-=2;
+        if(l[0] == sen-1){
+            l[1]++;
+        }
     }
-    else{
-        len = text[location[0]].length;
+    if (flag ==2){
+        l[1]++;
     }
-    holder.style.width = str(location[1]*100/len)+'%';
-    location[1]++;
-    if(location[1]>len){
-        location[1]-=2;
+    if(l[1]>len){
+        flag = 1;
     }
-    else if(location[1]<0){
-        location[1] = 0;
-        location[0]++;
-        holder.innerHTML = text[location[0]];
+    else if(l[1]<0){
+        flag = 0;
+        l[1] = 0;
+        l[0]++;
+        holder.innerHTML = text[l[0]];
     }
 }
-var myVar = setInterval(loop, 1);
+var myVar = setInterval(loop, 100);
