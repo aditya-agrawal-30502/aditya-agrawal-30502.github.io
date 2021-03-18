c57a1dc2293ce931ef6ceb44b24180efeda55124
aditya-agrawal-30502 <f20201381@goa.bits-pilani.ac.in>
2021-03-18 12:00:43 +0530

    cookied

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 var holder = document.getElementById('main_span');
 var text = ['Welcome to my webpage.','Scroll down for required links.','What are you waiting for?','Go.'];
+var bod = document.getElementById('bg');
+var x = document.cookie;
+if(x == ''){
+    console.log('enter')
+    var person = prompt("Please enter your name (We will remember you next time)", "Name");
+    if(person!= null){
+    person = 'Hello '+person+',';
+    document.cookie = "username="+person+"; expires=Thu, 18 Dec 2025 12:00:00 UTC";
+    }
+    else{
+        person = '';
+    }
+}
+else{
+    person = x.substring(9,x.length);
+}
+text = [person].concat(text);
+console.log(text);
 var t = text.length;
 var l = [0,0];
 var flag = 0;
 var c = 0;
 var f = 0;
 var sen = text.length;
-var bod = document.getElementById('bg')
 function loop(){
     var len = 0;
     len = text[l[0]].length;
